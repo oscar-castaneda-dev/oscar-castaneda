93b08aef71ac2545058b8f456c9cbd22d2253ae2
Oscar Castaneda <oscar.castaneda.developer@gmail.com>
2023-09-03 09:53:42 -0600

    feat: add SEO configuration

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import type { ISite } from "./types";
+
+export const SITE: ISite = {
+  author: "Oscar Castaneda",
+  coverImage: "cover.jpg",
+  description:
+    "Oscar Castaneda: A dedicated full stack developer, crafting seamless digital experiences through meticulous front-end and back-end work.",
+  title: "Oscar Castaneda",
+  website: "https://www.oscarcastaneda.dev/",
+};
